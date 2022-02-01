7059e639fe0b6cfbbb922c032bf21b0a5cafcc97
warleyeduardo <warleyeduardo@hotmail.com>
2022-02-01 15:12:25 -0300

    correção6

--- models/usuario.js
+++ models/usuario.js
@@ -53,25 +53,17 @@ UsuarioSchema.plugin(uniqueValitador, { message: 'já está sendo utilizado' });
 // criando a senha de usuario
 UsuarioSchema.methods.setSenha = function (password) {
 	this.salt = crypto.randomBytes(16).toString('hex');
-	this.hash = crypto.pbkdf2Sync(
-		password,
-		this.salt,
-		1000,
-		512,
-		'sha512'
-	).toString;
+	this.hash = crypto
+		.pbkdf2Sync(password, this.salt, 10000, 512, 'sha512')
+		.toString('hex');
 };
 
 // validando se senha é a mesma do usuario
 
-UsuarioSchema.methods.validaSenha = function (password) {
-	const hash = crypto.pbkdf2Sync(
-		password,
-		this.salt,
-		1000,
-		512,
-		'sha512'
-	).toString;
+UsuarioSchema.methods.validarSenha = function (password) {
+	const hash = crypto
+		.pbkdf2Sync(password, this.salt, 10000, 512, 'sha512')
+		.toString('hex');
 
 	return hash === this.hash;
 };
