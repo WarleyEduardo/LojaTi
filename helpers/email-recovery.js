83ff222c50387d4b03789ea569e819df83ca23f2
warleyeduardo <warleyeduardo@hotmail.com>
2022-02-05 11:21:53 -0300

    correção8

--- helpers/email-recovery.js
+++ helpers/email-recovery.js
@@ -2,7 +2,7 @@ const transporter = require('nodemailer').createTransport(
 	require('../config/email')
 );
 
-const { root: link } = require('../config/index');
+const { api: link } = require('../config/index');
 
 module.exports = ({ usuario, recovery }, cb) => {
 	const message = `<h1 style="text-align:center;">Recuperação de senha </h1>
